c93b5bbc29b9a43a678c555f21752bee4378dbf2
sartiniomar <sartiniomar@gmail.com>
2026-05-02 12:20:38 -0300

    Test123

--- src/main/java/com/sartiniomar/library/LibraryApplication.java
+++ src/main/java/com/sartiniomar/library/LibraryApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LibraryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LibraryApplication.class, args);
+    System.out.println("🔥 VERSION version123 🔥");
+    SpringApplication.run(LibraryApplication.class, args);
 	}
 
 }
