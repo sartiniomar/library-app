1c9df9ec1ee2176b8bac06e68245bc2bb0777e13
sartiniomar <sartiniomar@gmail.com>
2026-08-30 10:49:51 -0300

    OMAR-14 Implement Patron Loan Eligibility Rules

--- src/main/java/com/sartiniomar/library/loan/application/port/in/ReserveUseCase.java
+++ src/main/java/com/sartiniomar/library/loan/application/port/in/ReserveUseCase.java
@@ -2,6 +2,6 @@ package com.sartiniomar.library.loan.application.port.in;
 
 import com.sartiniomar.library.loan.domain.loan.Loan;
 
-public interface PlaceHoldUseCase {
-  Loan execute(PlaceHoldCommand command);
+public interface ReserveUseCase {
+  Loan execute(reserveCommand command);
 }
--- src/main/java/com/sartiniomar/library/loan/domain/loan/service/ReserveServiceDomain.java
+++ src/main/java/com/sartiniomar/library/loan/domain/loan/service/ReserveServiceDomain.java
@@ -2,12 +2,9 @@ package com.sartiniomar.library.loan.domain.loan.service;
 
 import com.sartiniomar.library.loan.domain.bookInstance.BookInstance;
 import com.sartiniomar.library.loan.domain.loan.DomainPolicy;
-import com.sartiniomar.library.loan.domain.loan.DomainResult;
 import com.sartiniomar.library.loan.domain.loan.Loan;
-import com.sartiniomar.library.loan.domain.loan.LoanBookEvent;
 import com.sartiniomar.library.loan.domain.patron.Patron;
 import java.time.Clock;
-import java.util.List;
 
 public class ReserveServiceDomain {
 
@@ -17,12 +14,11 @@ public class ReserveServiceDomain {
     this.clock = clock;
   }
 
-  public DomainResult<Loan> reserve(Patron patron, BookInstance bookInstance) {
+  public Loan reserve(Patron patron, BookInstance bookInstance) {
     DomainPolicy.ensurePatronCanLoanBook(patron, bookInstance);
     bookInstance.ensureCanBeReserved();
     Loan reserve = Loan.createReserve(patron.getId(), bookInstance.getId(), clock);
     bookInstance.reserved();
-    LoanBookEvent event = new LoanBookEvent(patron.getId(), bookInstance.getId(), reserve.getStatus().toString());
-    return new DomainResult<>(reserve, List.of(event));
+    return reserve;
   }
 }
